bf609a8233d74b0849047173a84b9f765942357b
Matheuspz <mpzenere@gmail.com>
2023-06-17 11:59:14 -0300

    Mudança de estilo do index

--- javacript/carousel.js
+++ javacript/carousel.js
@@ -1,27 +1,25 @@
 
+let slideIndex = 1;
+showSlides(slideIndex);
 
-let slideIndex = [1, 2];
-/* Class the members of each slideshow group with different CSS classes */
-let slideId = ["mySlides", "mySlidesQDR", "mySlidesSUR"];
-showSlides(1, 0);
-showSlides(1, 1);
-showSlides(1, 2);
-
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function showSlides(n, no) {
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
   let i;
-  let x = document.getElementsByClassName(slideId[no]);
-
-  document.getElementById("sp_img_txt").style.display = "none";
-  document.getElementById("qdr_txt_del").style.display = "none";
-
-  if (n > x.length) {slideIndex[no] = 1}
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+  let slides = document.getElementsByClassName("mySlides");
+  document.getElementById("templateQdr").style.display = "none";
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-  x[slideIndex[no]-1].style.display = "block";
+  slides[slideIndex-1].style.display = "block";
 }
