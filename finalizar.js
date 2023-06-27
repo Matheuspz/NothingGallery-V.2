52a9a2f04327cb6ebbb42788ce39016f11d0b60f
Matheuspz <mpzenere@gmail.com>
2023-06-27 12:27:03 -0300

    terminar php finalizar compra e sistema de login..

---
+++ finalizar.js
@@ -0,0 +1,41 @@
+function pagarCartao() {
+    document.getElementById("metodoPag").style.display = "none";
+    document.getElementById("metodoPag2").style.display = "none";
+    document.getElementById("metodoPag3").style.display = "none";
+    document.getElementById("formC").style.display = "flex";
+    document.getElementById("btnVoltar").style.display = "block";
+    document.getElementById('titulo').innerHTML = "Cartão";
+    document.getElementById('titulo').style.width = "15vmin";
+}
+function pagVoltar() {
+    document.getElementById("metodoPag").style.display = "flex";
+    document.getElementById("metodoPag2").style.display = "flex";
+    document.getElementById("metodoPag3").style.display = "flex";
+    document.getElementById("formC").style.display = "none";
+    document.getElementById("comprarPix").style.display = "none";
+    document.getElementById("btnVoltar").style.display = "none";
+    document.getElementById('titulo').innerHTML = "Selecione a forma de pagamento";
+    document.getElementById('titulo').style.width = "";
+}
+function pagarPix() {
+    document.getElementById("metodoPag").style.display = "none";
+    document.getElementById("metodoPag2").style.display = "none";
+    document.getElementById("metodoPag3").style.display = "none";
+    document.getElementById("comprarPix").style.display = "flex";
+    document.getElementById("btnVoltar").style.display = "block";
+    document.getElementById('titulo').innerHTML = "Pix";
+    document.getElementById('titulo').style.width = "10vmin";
+}
+function finalizarPix() {
+    document.getElementById("comprarPix").style.display = "none";
+    document.getElementById("btnVoltar").style.display = "none";
+    document.getElementById('titulo').innerHTML = "Leia QRCODE da chave Pix";
+    document.getElementById('qrcode').style.display = "flex"
+    document.getElementById('titulo').style.width = "";
+
+}
+
+function Validar() {
+    let regexNumC = /^.{13,16}$/gm;
+    let regexNome = /^[a-z]+\ [a-z]+$/gm;
+}
